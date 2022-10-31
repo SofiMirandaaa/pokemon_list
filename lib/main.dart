9581a7a11e71c185fd389906ad1696760ba877ad
Sofi Miranda <sofi.miranda.ft21@mail.umy.ac.id>
2022-10-31 22:44:09 +0700

    add class scoreboard stf

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class scoreboard extends StatefulWidget {
+  const scoreboard({super.key});
+
+  @override
+  State<scoreboard> createState() => _scoreboardState();
+}
+
+class _scoreboardState extends State<scoreboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
